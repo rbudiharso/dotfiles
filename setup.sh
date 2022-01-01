b84f295c18e43407484180cd6fb9290afa7a008b
Rahmat Budiharso <rbudiharso@gmail.com>
2022-01-02 06:30:46 +0700

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 set -x
 
 # Manual
-# download dotfiles from gdrive and put them in homedir except config
+# download dotfiles from gdrive and put them in $HOME except config
 
 # Automatic
 chmod 600 $HOME/.kube/config
@@ -13,9 +13,6 @@ mkdir -p $HOME/.local/share/applications
 
 git config --global user.email "rbudiharso@gmail.com"
 git config --global user.name "Rahmat Budiharso"
-
-# Slack
-curl -Lso /tmp/slack.rpm https://downloads.slack-edge.com/linux_releases/slack-4.11.3-0.1.fc21.x86_64.rpm
 
 cd $HOME/Downloads
 # Fonts with ligature
@@ -33,6 +30,12 @@ mkdir -p $HOME/.local/share/fonts
 mv *.ttf $HOME/.local/share/fonts
 rm -rf ./*
 
+# Slack
+curl -Lso /tmp/slack.rpm https://downloads.slack-edge.com/releases/linux/4.23.0/prod/x64/slack-4.23.0-0.1.fc21.x86_64.rpm
+
+# K8s Lens
+curl -Lso /tmp/lens.rpm https://api.k8slens.dev/binaries/Lens-5.3.3-latest.20211223.1.x86_64.rpm
+
 # onefetch
 cd /tmp
 curl -Lso /tmp/onefetch.tar.gz https://github.com/o2sh/onefetch/releases/download/v2.8.0/onefetch-linux.tar.gz
@@ -42,10 +45,13 @@ mv /tmp/onefetch $HOME/.local/bin/onefetch
 # a bunch of sudo commands
 sudo fc-cache -fv $HOME/.local/share/fonts
 
-sudo dnf update -y
-sudo dnf install -y neovim zsh stow fzf tmux vifm kitty alacritty sway swaylock swayidle bat wofi grim slurp waybar libnsl wl-clipboard xclip awscli mako curl git util-linux-user neofetch gnome-shell-extension-user-theme wlogout gnome-tweaks light playerctl flatpak NetworkManager-tui openssl-devel readline-devel zlib-devel gcc-c++ polkit-gnome kanshi swappy pngquant pulseaudio-utils
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf update -y
+sudo dnf install -y neovim zsh stow fzf tmux vifm kitty alacritty sway swaylock swayidle bat wofi grim slurp waybar libnsl wl-clipboard xclip awscli mako curl git util-linux-user neofetch gnome-shell-extension-user-theme wlogout gnome-tweaks light playerctl flatpak NetworkManager-tui openssl-devel readline-devel zlib-devel gcc-c++ polkit-gnome kanshi swappy pngquant pulseaudio-utils telegram-desktop mozilla-fira-sans-fonts
 sudo dnf install -y /tmp/slack.rpm
+sudo dnf install -y /tmp/lens.rpm
 sudo flatpak install -y flathub com.spotify.Client
 
 cd $HOME
@@ -73,44 +79,6 @@ curl -Lso $HOME/.config/zsh/.zim/zimfw.zsh https://github.com/zimfw/zimfw/releas
 zsh ~/.config/zsh/.zim/zimfw.zsh install
 
 nvim +PlugInstall +qall
-
-cd $HOME/Downloads
-# Telegram
-curl -Lso telegram.desktop.tar.xz https://telegram.org/dl/desktop/linux
-tar -xf telegram.desktop.tar.xz
-mv Telegram/Telegram $HOME/.local/bin/Telegram
-chmod +x $HOME/.local/bin/Telegram
-cat > $HOME/.local/share/applications/Telegram.desktop <<END
-[Desktop Entry]
-Version=1.0
-Name=Telegram Desktop
-Comment=Official desktop version of Telegram messaging app
-TryExec=/home/rbudiharso/.local/bin/Telegram
-Exec=/home/rbudiharso/.local/bin/Telegram -workdir /home/rbudiharso/.local/share/TelegramDesktop/ -- %u
-Icon=telegram
-Terminal=false
-StartupWMClass=TelegramDesktop
-Type=Application
-Categories=Chat;Network;InstantMessaging;Qt;
-MimeType=x-scheme-handler/tg;
-Keywords=tg;chat;im;messaging;messenger;sms;tdesktop;
-X-GNOME-UsesNotifications=true
-END
-
-# Lens
-curl -Lso $HOME/.local/bin/Lens.AppImage https://github.com/lensapp/lens/releases/download/v4.0.6/Lens-4.0.6.AppImage
-chmod +x $HOME/.local/bin/Lens.AppImage
-cat > $HOME/.local/share/applications/Lens.desktop <<END
-[Desktop Entry]
-Version=1.0
-Name=Lens Kubernetes Dashboard
-TryExec=/home/rbudiharso/.local/bin/Lens.AppImage
-Exec=/home/rbudiharso/.local/bin/Lens.AppImage
-Icon=Lens
-Terminal=false
-StartupWMClass=LensDashboard
-Type=Application
-END
 
 # better git diff
 git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX"
