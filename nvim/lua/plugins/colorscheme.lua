ffffcd9d41eb557248e26628e1c124938992683d
Rahmat Budiharso <rbudiharso@gmail.com>
2024-06-06 22:20:20 +0700

    add lazyvim

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,30 @@
+return {
+  {
+    "ellisonleao/gruvbox.nvim",
+    opts = {
+      transparent = true,
+    },
+  },
+  {
+    "tokyonight.nvim",
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
+  {
+    "catppuccin",
+    opts = {
+      transparent_background = true,
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
